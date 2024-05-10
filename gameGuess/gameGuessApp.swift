dc5a971d0c27af042db6e30e7265eb71fa418c54
Civan Metin <146356511+Gone-M@users.noreply.github.com>
2024-05-10 14:14:35 -0400

    Initial Commit

---
+++ gameGuess/gameGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameGuessApp.swift
+//  gameGuess
+//
+//  Created by Civan Metin on 2024-05-10.
+//
+
+import SwiftUI
+
+@main
+struct gameGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
